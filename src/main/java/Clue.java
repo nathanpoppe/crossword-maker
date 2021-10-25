3d599064b4446896b56f7f06499355fc1f6a7dba
nathanpoppe <nathanpoppe16@gmail.com>
2021-10-25 10:35:36 -0500

    Changed filepath name to java files.

---
+++ src/main/java/Clue.java
@@ -0,0 +1,29 @@
+
+/**
+ *  CLASS: Clue
+ *  PURPOSE: Each made crossword consists of 2 ArrayLists of clues (across/down)
+ *  AUTHOR: Nathan Poppe
+ */
+public class Clue {
+
+    private String description; // the "actual" clue
+    private int number; // the corresponding clue number
+
+    public Clue(String description, int number){
+        this.description = description;
+        this.number = number;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+}
